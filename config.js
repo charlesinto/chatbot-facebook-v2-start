58c392bf759a78bcfb07d81e855cb997ec2f340b
charlesinto <charles.onuorah@yahoo.com>
2020-07-23 00:11:23 +0100

    first commit

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
+    FB_PAGE_TOKEN: 'EAAEgMzUW6GQBAPJZBFSp1WqCUdlK9aNmQODx3mK0lXVRIHSZCikeXELMxX552LGXaHk9Uc85aZBiQ6RKWhZBZCgR49YdyK3EjdOMZBZC7ZA5M3G5slZAnwDnWP5DPeiW7B3QtCSWW06XUPBZBOePbfmkaMkQO3UXz6vPl92ds59trpjotiUMshpxsD',
+    FB_VERIFY_TOKEN: 'myugonmaisawesomepersonthatilove',
     FB_APP_SECRET: '',
-    SERVER_URL: '',
+    SERVER_URL: 'https://ugonma-bot.herokuapp.com/',
     GOOGLE_PROJECT_ID: '',
     DF_LANGUAGE_CODE: '',
     GOOGLE_CLIENT_EMAIL: '',
